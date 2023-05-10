6c5352204a8ba2bb1edf56622b6b6581b2bf9e35
andretassis <tassisandre@gmail.com>
2023-05-10 17:14:34 -0300

    Criado o routes e tentando fazer as rotas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Aulas from './componentes/aulas/aulas';
 import Horario from './componentes/horario/horario';
 import Nav from './componentes/nav/nav';
 import Precos from './componentes/precos/precos';
+import Login from './componentes/login/login'
+
 
 
 function App() {
   return (
     <>
-      <Nav></Nav>
-      <Aulas></Aulas>
-      <Precos></Precos>
-      <Horario></Horario>
+      <Router>
+        <Switch>
+          <Nav></Nav>
+          <Aulas></Aulas>
+          <Precos></Precos>
+          <Horario></Horario>
+          <Login />
+        </Switch>
+      </Router>
     </>
   );
 }
---
+++ src/componentes/routes/routes.js
@@ -0,0 +1,13 @@
+
+
+//import Login from "../login/login"
+//
+//const Routes = () => {
+//    return (
+//        <Router>
+//            <Route component={Login} path="/login" />
+//        </Router>
+//    )
+//}
+//
+//export default Routes
