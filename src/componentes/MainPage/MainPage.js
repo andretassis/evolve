3af6741c1116899c6339db2f8ce24bb8d0e305ba
Andre Tassis <tassisandre@gmail.com>
2023-05-12 18:51:28 -0300

    Alterações em rotas e criação de Nav.js

--- src/componentes/MainPage/MainPage.js
+++ src/componentes/MainPage/MainPage.js
@@ -8,25 +8,11 @@ import check from '../../imagens/check-circle.svg'
 import negative from '../../imagens/x-circle.svg'
 import './MainPage.css'
 
+
 function MainPage() {
+
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <a className="navbar-brand titulo" href="#"><strong>Evolve</strong> Fitness Studio</a>
-                <div className="container-fluid">
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div className="navbar-nav">
-                            <a className="nav-link" aria-current="page" href="#">Aulas</a>
-                            <a className="nav-link" href="#">Planos</a>
-                            <a className="nav-link" href="#">Horário</a>
-                            <a className="nav-link" href="#">Área do aluno</a>
-                        </div>
-                    </div>
-                </div>
-            </nav>
 
             <div className="aulas-titulo">
                 <p>Nossas <strong>Aulas</strong></p>
